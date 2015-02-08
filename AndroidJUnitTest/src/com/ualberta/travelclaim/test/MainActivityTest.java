1d026e5f660e258c7eba659f7ceb1762dca44662
denweny <dtruong1@ualberta.ca>
2015-02-08 16:09:58 -0700

    TDD test
    tdd

--- AndroidJUnitTest/src/com/ualberta/travelclaim/test/MainActivityTest.java
+++ AndroidJUnitTest/src/com/ualberta/travelclaim/test/MainActivityTest.java
@@ -1,5 +1,20 @@
 package com.ualberta.travelclaim.test;
 
-public class MainActivityTest {
+import android.test.ActivityInstrumentationTestCase2;
+
+public class MainActivityTest extends
+		ActivityInstrumentationTestCase2<TravelClaim> {
+
+	public MainActivityTest(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
 
 }
