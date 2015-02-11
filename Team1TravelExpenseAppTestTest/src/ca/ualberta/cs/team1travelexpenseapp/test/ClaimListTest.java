1066e1680843b1581348af56e37692950c9edf40
Mike <kjyoung@ualberta.ca>
2015-02-10 19:41:17 -0700

    Added test project

---
+++ Team1TravelExpenseAppTestTest/src/ca/ualberta/cs/team1travelexpenseapp/test/ClaimListTest.java
@@ -0,0 +1,11 @@
+package ca.ualberta.cs.team1travelexpenseapp.test;
+
+import junit.framework.TestCase;
+
+public class ClaimListTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+}
