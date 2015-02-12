91a4d7be5d31dedc53f5a16c9665f3ba1da76836
Mike <kjyoung@ualberta.ca>
2015-02-12 16:57:26 -0700

    prepull commit

--- Team1TEAppTest/src/ca/ualberta/cs/team1travelexpenseapp/test/TagsEditTest.java
+++ Team1TEAppTest/src/ca/ualberta/cs/team1travelexpenseapp/test/TagsEditTest.java
@@ -1,15 +1,27 @@
 package ca.ualberta.cs.team1travelexpenseapp.test;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.widget.ListView;
 import junit.framework.TestCase;
 
 public class TagsEditTest extends TestCase {
-
-	protected void setUp() throws Exception {
-		super.setUp();
+	Activity activity;
+	ListView tagListView;
+	
+	public ClaimantClaimsListTest() {
+		super(MainActivity.class);
 	}
 	
-	//US03.01.01:As a claimant, I want to give an expense claim one or more alphanumeric 
-	//tags, so that claims can be organized by me into groups.
+	protected void setUp() throws Exception {
+		super.setUp();
+		Intent intent = new Intent();
+		setActivityIntent(intent);
+		activity = getActivity();
+		tagListView = (ListView) (activity.findViewById(ca.ualberta.cs.team1travelexpenseapp.R.id.tagsList));
+	}
+	
+	
 	
 	//US03.02.01: As a claimant, I want to manage my personal use of tags by listing 
 	//the available tags, adding a tag, renaming a tag, and deleting a tag.
