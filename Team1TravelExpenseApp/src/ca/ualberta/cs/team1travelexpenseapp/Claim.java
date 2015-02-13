8408efc1101b69d710332978fc4133aad1db5e25
aylmao <aylmao@users.noreply.github.com>
2015-02-12 20:31:28 -0700

    US08.03.01 is finished

--- Team1TravelExpenseApp/src/ca/ualberta/cs/team1travelexpenseapp/Claim.java
+++ Team1TravelExpenseApp/src/ca/ualberta/cs/team1travelexpenseapp/Claim.java
@@ -73,7 +73,7 @@ public class Claim {
 		return false;
 	}
 
-	public boolean addDestination() {
+	public boolean addDestination(String destination) {
 		// TODO Auto-generated method stub
 		return false;
 	}
@@ -91,6 +91,11 @@ public class Claim {
 	public boolean setToDate() {
 		// TODO Auto-generated method stub
 		return false;
+	}
+	
+	public void addApprover(String string) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 	public String toString(){
