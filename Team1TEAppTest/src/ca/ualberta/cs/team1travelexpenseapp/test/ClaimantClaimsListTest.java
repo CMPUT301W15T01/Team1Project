e56297fd8a3bfd9cc1d104dca3d2ca9eaa171294
Mike <kjyoung@ualberta.ca>
2015-02-12 17:05:34 -0700

    prepull commit

--- Team1TEAppTest/src/ca/ualberta/cs/team1travelexpenseapp/test/ClaimantClaimsListTest.java
+++ Team1TEAppTest/src/ca/ualberta/cs/team1travelexpenseapp/test/ClaimantClaimsListTest.java
@@ -2,9 +2,11 @@ package ca.ualberta.cs.team1travelexpenseapp.test;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import ca.ualberta.cs.team1travelexpenseapp.Claim;
+import ca.ualberta.cs.team1travelexpenseapp.ClaimsListController;
 import ca.ualberta.cs.team1travelexpenseapp.MainActivity;
 import ca.ualberta.cs.team1travelexpenseapp.R;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.widget.ListView;
@@ -28,7 +30,6 @@ public class ClaimantClaimsListTest extends ActivityInstrumentationTestCase2<Mai
 		setActivityIntent(intent);
 		activity = getActivity();
 		claimListView = (ListView) (activity.findViewById(ca.ualberta.cs.team1travelexpenseapp.R.id.claimsList));
-		/*
 		Claim claim1 = new Claim("name",new Date(2000,11,11), new Date(2015,12,12));
 		Claim claim2 = new Claim("name",new Date(1990,1,8), new Date(2000,12,12));
 		Claim claim3 = new Claim("name",new Date(1999,9,8), new Date(2012,12,12));
@@ -39,7 +40,6 @@ public class ClaimantClaimsListTest extends ActivityInstrumentationTestCase2<Mai
 		ClaimsListController.addClaim(claim3);
 		ClaimsListController.addClaim(claim4);
 		ClaimsListController.addClaim(claim5);	
-		*/
 	}
 
 	
