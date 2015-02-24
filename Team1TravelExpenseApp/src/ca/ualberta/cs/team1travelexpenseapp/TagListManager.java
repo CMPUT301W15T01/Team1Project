59ed7e5f0a78c7d7a77fb55aede29e3332548498
Mike <kjyoung@ualberta.ca>
2015-02-24 16:39:29 -0700

    tried to implement listener and use it in TagListController

---
+++ Team1TravelExpenseApp/src/ca/ualberta/cs/team1travelexpenseapp/TagListManager.java
@@ -0,0 +1,21 @@
+package ca.ualberta.cs.team1travelexpenseapp;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+
+public class TagListManager {
+	public void saveTags(ArrayList<Tag> tagList){
+		
+	}
+	
+	public ArrayList<Tag> loadTags(){
+		return null;
+		
+	}
+
+	public static void initManager(Context applicationContext) {
+		// TODO Auto-generated method stub
+		
+	}
+}
