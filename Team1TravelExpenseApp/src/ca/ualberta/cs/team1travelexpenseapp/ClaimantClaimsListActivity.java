e8c507d6d3fa7fbef16ea098ae25f35676c80b81
aylmao <aylmao@users.noreply.github.com>
2015-02-27 16:52:57 -0700

    changed context of appliaction to team1travelexpenseapp and tested findviewbyid

--- Team1TravelExpenseApp/src/ca/ualberta/cs/team1travelexpenseapp/ClaimantClaimsListActivity.java
+++ Team1TravelExpenseApp/src/ca/ualberta/cs/team1travelexpenseapp/ClaimantClaimsListActivity.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class ClaimantClaimsListActivity extends Activity {
 
