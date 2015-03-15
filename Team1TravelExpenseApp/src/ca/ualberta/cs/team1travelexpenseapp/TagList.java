a0f05148354b34ba799a19f867d4d3f0e224ba4d
Mike <kjyoung@ualberta.ca>
2015-03-15 10:38:19 -0600

    Added basic javadocs to tag related stuff.

--- Team1TravelExpenseApp/src/ca/ualberta/cs/team1travelexpenseapp/TagList.java
+++ Team1TravelExpenseApp/src/ca/ualberta/cs/team1travelexpenseapp/TagList.java
@@ -1,7 +1,11 @@
 package ca.ualberta.cs.team1travelexpenseapp;
 
 import java.util.ArrayList;
-
+/**
+ * Stores a list of tags along with a set of listeners to be updated whenever the list is modified.
+ * Will eventually provide an interface to a TagListManager which will save the list to disk and the web when connected.
+ *
+ */
 public class TagList {
 	private ArrayList<Tag> tagList;
 	private ArrayList<Listener> listeners;
@@ -13,28 +17,49 @@ public class TagList {
 		listeners=new ArrayList<Listener>();
 	}
 	
+	/**
+	 * Save the stored tagList to disk/the web server (not yet implemented).
+	 */
 	public void saveTagList(){
 		manager.saveTags();
 	}
-	
+	/**
+	 * Add a new listener to be updated whenever the tagList is changed.
+	 * @param listener
+	 */
 	public void addListener(Listener listener){
 		listeners.add(listener);
 	}
 	
+	/**
+	 * Remove a listener so that it will no longer be updated when the tagList is changed.
+	 * @param listener
+	 */
 	public void removeListener(Listener listener){
 		listeners.remove(listener);
 	}
 	
+	/**
+	 * Call update method on all listeners (called on tagList changes).
+	 */
 	private void notifyListeners(){
 		for(int i=0; i<listeners.size();i++){
 			listeners.get(i).update();
 		}
 	}
 
+	/**
+	 * Returns the stored tagList.
+	 * @return
+	 */
 	public ArrayList<Tag> getTags() {
 		return tagList;
 	}
 
+	/**
+	 * Set the stored tagList, will automatically update listeners and save the list once saving is implemented.
+	 * @param tagList
+	 */
 	public void setTagList(ArrayList<Tag> tagList) {
 		this.tagList = tagList;
 		notifyListeners();
