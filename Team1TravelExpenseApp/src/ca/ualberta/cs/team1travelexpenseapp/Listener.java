80bca2977d7befde02c1b6e1edd3c276719a1f31
aylmao <aylmao@users.noreply.github.com>
2015-03-16 09:59:29 -0600

    license

--- Team1TravelExpenseApp/src/ca/ualberta/cs/team1travelexpenseapp/Listener.java
+++ Team1TravelExpenseApp/src/ca/ualberta/cs/team1travelexpenseapp/Listener.java
@@ -1,3 +1,17 @@
+/*
+Copyright 2015 Jeffrey Oduro, Cody Ingram, Boyan Peychoff, Kenny Young, Dennis Truong, Victor Olivares 
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package ca.ualberta.cs.team1travelexpenseapp;
 /**
  * Listener interface, listeners will be placed in views and passed to model objects to allow the model to call update in the listener,
