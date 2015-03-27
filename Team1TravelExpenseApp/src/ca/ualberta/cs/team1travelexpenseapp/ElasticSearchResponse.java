b13d591b2ca7a25f2e00867f77a7d2022640fd99
Mike <kjyoung@ualberta.ca>
2015-03-27 17:09:07 -0600

    added loadClaimsFromWeb to claimListManager function, likely doesn't quite work yet

---
+++ Team1TravelExpenseApp/src/ca/ualberta/cs/team1travelexpenseapp/ElasticSearchResponse.java
@@ -0,0 +1,15 @@
+//from https://github.com/rayzhangcl/ESDemo March 27 2015
+package ca.ualberta.cs.team1travelexpenseapp;
+
+public class ElasticSearchResponse<T> {
+    String _index;
+    String _type;
+    String _id;
+    int _version;
+    boolean exists;
+    T _source;
+    double max_score;
+    public T getSource() {
+        return _source;
+    }
+}
