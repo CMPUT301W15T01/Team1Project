10f5a88f188d120ff84c979d4ff5e8f6335583bc
aylmao <aylmao@users.noreply.github.com>
2015-03-27 02:26:53 -0600

    small temp test

---
+++ Team1TravelExpenseApp/src/ca/ualberta/cs/team1travelexpenseapp/claims/SmallClaimTest.java
@@ -0,0 +1,19 @@
+package ca.ualberta.cs.team1travelexpenseapp.claims;
+
+import junit.framework.TestCase;
+
+public class SmallClaimTest extends TestCase {
+
+	ProgressClaim progressClaim;
+	
+	public SmallClaimTest() {
+		super();
+		progressClaim = new ProgressClaim();
+	}
+	
+	public void testChangeStatus() {
+		assertTrue(true);
+		}
+	
+
+}
