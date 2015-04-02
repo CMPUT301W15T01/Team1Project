5a4794675a0be9cbaf7fc68d08fb45213315d8b3
Mike <kjyoung@ualberta.ca>
2015-04-02 15:09:50 -0600

    forgot to add new class sorry

---
+++ Team1TravelExpenseApp/src/ca/ualberta/cs/team1travelexpenseapp/Destination.java
@@ -0,0 +1,40 @@
+package ca.ualberta.cs.team1travelexpenseapp;
+
+import android.location.Location;
+
+public class Destination {
+	private String name;
+	private String reason;
+	private Location location;
+	
+	Destination(String name, String reason, Location location){
+		this.name = name;
+		this.reason = reason;
+		this.location = location;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getReason() {
+		return reason;
+	}
+
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+
+	public Location getLocation() {
+		return location;
+	}
+
+	public void setLocation(Location location) {
+		this.location = location;
+	}
+	
+}
