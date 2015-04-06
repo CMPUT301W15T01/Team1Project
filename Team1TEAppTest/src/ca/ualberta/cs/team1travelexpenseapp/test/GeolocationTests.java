86e5c687efd7e9a665fee5c41207020a005fe896
denweny <dtruong1@ualberta.ca>
2015-04-05 19:58:59 -0600

    Finished geolocation tests

--- Team1TEAppTest/src/ca/ualberta/cs/team1travelexpenseapp/test/GeolocationTests.java
+++ Team1TEAppTest/src/ca/ualberta/cs/team1travelexpenseapp/test/GeolocationTests.java
@@ -1,15 +1,21 @@
 package ca.ualberta.cs.team1travelexpenseapp.test;
 
+import org.osmdroid.views.MapView;
+
 import testObjects.MockClaimant;
+import android.app.Dialog;
 import android.app.Instrumentation;
 import android.app.Instrumentation.ActivityMonitor;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.location.Address;
 import android.location.Location;
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.ViewAsserts;
 import android.util.Log;
+import android.view.ViewStub;
 import android.widget.Button;
 import android.widget.ListView;
-import ca.ualberta.cs.team1travelexpenseapp.ApproverExpenseListActivity;
 import ca.ualberta.cs.team1travelexpenseapp.ClaimListController;
 import ca.ualberta.cs.team1travelexpenseapp.ClaimantClaimsListActivity;
 import ca.ualberta.cs.team1travelexpenseapp.ClaimantExpenseListActivity;
@@ -18,6 +24,7 @@ import ca.ualberta.cs.team1travelexpenseapp.EditClaimActivity;
 import ca.ualberta.cs.team1travelexpenseapp.EditExpenseActivity;
 import ca.ualberta.cs.team1travelexpenseapp.Expense;
 import ca.ualberta.cs.team1travelexpenseapp.ExpenseListController;
+import ca.ualberta.cs.team1travelexpenseapp.LoginActivity;
 import ca.ualberta.cs.team1travelexpenseapp.OSMDroidMapActivity;
 import ca.ualberta.cs.team1travelexpenseapp.R;
 import ca.ualberta.cs.team1travelexpenseapp.claims.Claim;
@@ -25,7 +32,7 @@ import ca.ualberta.cs.team1travelexpenseapp.singletons.UserSingleton;
 import ca.ualberta.cs.team1travelexpenseapp.users.Claimant;
 
 public class GeolocationTests extends
-		ActivityInstrumentationTestCase2<ClaimantClaimsListActivity> {
+		ActivityInstrumentationTestCase2<LoginActivity> {
 	protected Instrumentation instrumentation;
 	protected ExpenseListController ExpenseListController;
 	protected ClaimListController ClaimListController;
@@ -34,13 +41,14 @@ public class GeolocationTests extends
 	protected EditClaimActivity editClaimActivity;
 	protected EditExpenseActivity editExpenseActivity;
 	protected OSMDroidMapActivity osmDroidMapActivity;
-	
+	protected LoginActivity loginActivity;
+
 	protected Claimant user;
 	protected Expense expense;
 	protected Claim claim;
-	
+
 	public GeolocationTests() {
-		super(ClaimantClaimsListActivity.class);
+		super(LoginActivity.class);
 	}
 
 	protected void setUp() throws Exception {
@@ -54,56 +62,83 @@ public class GeolocationTests extends
 		Log.d("Geolocationtest", "User Singleton set");
 		ClaimListController = new ClaimListController(user.getClaimList());
 
+		loginActivity = getActivity();
 		user.getClaimList().getManager()
-				.setContext(getActivity().getApplicationContext());
+				.setContext(loginActivity.getApplicationContext());
 
 		Log.d("Geolocationtest",
 				"ClaimList has a manager? "
 						+ String.valueOf(user.getClaimList().getManager() != null));
 		instrumentation = getInstrumentation();
-		
+
 		instrumentation.runOnMainSync(new Runnable() {
 			@Override
 			public void run() {
 				claim = new Claim();
-				claim.addDestination(new Destination("name", "reason", new Location("")));
+				claim.addDestination(new Destination("name", "reason",
+						new Location("")));
 				ClaimListController.addClaim(claim);
 				ClaimListController.updateCurrentClaim(claim);
+				ClaimListController.changeClaim(claim);
 
 				ExpenseListController = new ExpenseListController(claim
 						.getExpenseList());
 				expense = new Expense();
 				expense.setLocation(new Location(""));
-				claim.getExpenseList().getExpenses()
-						.add(expense);
+				claim.getExpenseList().getExpenses().add(expense);
+				ExpenseListController.setCurrentExpense(expense);
+
 			}
 		});
 
 		instrumentation.waitForIdleSync();
-		
-		claimantClaimsListActivity = getActivity();
+
 	}
 
 	protected void tearDown() throws Exception {
+
+		if (osmDroidMapActivity != null) {
+			osmDroidMapActivity.finish();
+		}
+		if (editClaimActivity != null) {
+			editClaimActivity.finish();
+		}
+		if (editExpenseActivity != null) {
+			editExpenseActivity.finish();
+		}
+		if (claimantExpenseListActivity != null) {
+			claimantExpenseListActivity.finish();
+		}
+		if (claimantClaimsListActivity != null) {
+			claimantClaimsListActivity.finish();
+		}
+		if (loginActivity != null) {
+			loginActivity.finish();
+		}
 		super.tearDown();
-		 if (claimantClaimsListActivity != null) {
-		 claimantClaimsListActivity.finish();
-		 }
-		 if (claimantExpenseListActivity != null) {
-		 claimantExpenseListActivity.finish();
-		 }
-		 if (osmDroidMapActivity != null) {
-		 osmDroidMapActivity.finish();
-		 }			 
-		 if (editClaimActivity!= null) {
-			 editClaimActivity.finish();
-		 }
-		 if (editExpenseActivity!= null) {
-			 editExpenseActivity.finish();
-		 }
+
+	}
+
+	public void getLoginActivity() {
+		ActivityMonitor activityMonitor = instrumentation.addMonitor(
+				ClaimantClaimsListActivity.class.getName(), null, false);
+		final Button button = (Button) loginActivity
+				.findViewById(R.id.userButton);
+		instrumentation.runOnMainSync(new Runnable() {
+			@Override
+			public void run() {
+				button.performClick();
+			}
+		});
+		instrumentation.waitForIdleSync();
+		claimantClaimsListActivity = (ClaimantClaimsListActivity) instrumentation
+				.waitForMonitorWithTimeout(activityMonitor, 10000);
+		assertNotNull(claimantClaimsListActivity);
+
 	}
 
 	public void getAddDestination() {
+		getLoginActivity();
 		ActivityMonitor activityMonitor = instrumentation.addMonitor(
 				EditClaimActivity.class.getName(), null, false);
 		final Button button = (Button) claimantClaimsListActivity
@@ -119,10 +154,11 @@ public class GeolocationTests extends
 		editClaimActivity = (EditClaimActivity) instrumentation
 				.waitForMonitorWithTimeout(activityMonitor, 10000);
 		assertNotNull(editClaimActivity);
-		
+
 	}
 
 	public void getExpenseListActivity() {
+		getLoginActivity();
 		ActivityMonitor activityMonitor = instrumentation.addMonitor(
 				ClaimantExpenseListActivity.class.getName(), null, false);
 		final ListView listOfClaims = (ListView) claimantClaimsListActivity
@@ -148,7 +184,7 @@ public class GeolocationTests extends
 				EditExpenseActivity.class.getName(), null, false);
 		final Button button = (Button) claimantExpenseListActivity
 				.findViewById(R.id.addExpenseButton);
-
+		assertNotNull(button);
 		instrumentation.runOnMainSync(new Runnable() {
 			@Override
 			public void run() {
@@ -159,7 +195,7 @@ public class GeolocationTests extends
 		editExpenseActivity = (EditExpenseActivity) instrumentation
 				.waitForMonitorWithTimeout(activityMonitor, 10000);
 		assertNotNull(editExpenseActivity);
-		
+
 	}
 
 	/*
@@ -168,50 +204,242 @@ public class GeolocationTests extends
 	 */
 	public void testAddGeolocationToDestination() {
 		getAddDestination();
-		
+
+		final Button button = (Button) editClaimActivity
+				.findViewById(R.id.addDestinationButton);
+
+		instrumentation.runOnMainSync(new Runnable() {
+			@Override
+			public void run() {
+				button.performClick();
+			}
+		});
+		instrumentation.waitForIdleSync();
+
+		ActivityMonitor osm = instrumentation.addMonitor(
+				OSMDroidMapActivity.class.getName(), null, false);
+
+		ActivityMonitor activityMonitor = instrumentation.addMonitor(
+				EditClaimActivity.class.getName(), null, false);
+		Dialog dest = editClaimActivity.newDestDialog;
+		assertNotNull(dest);
+		assertTrue(dest.isShowing());
+
+		final Button buttonlocation = (Button) dest
+				.findViewById(R.id.selectLocationButton);
+
+		instrumentation.runOnMainSync(new Runnable() {
+			@Override
+			public void run() {
+				buttonlocation.performClick();
+			}
+		});
+		instrumentation.waitForIdleSync();
+
+		osmDroidMapActivity = (OSMDroidMapActivity) instrumentation
+				.waitForMonitorWithTimeout(osm, 10000);
+
+		assertNotNull(osmDroidMapActivity);
+
+		final Button buttonSetlocation = (Button) osmDroidMapActivity
+				.findViewById(R.id.gps);
+
+		instrumentation.runOnMainSync(new Runnable() {
+			@Override
+			public void run() {
+				buttonSetlocation.performClick();
+			}
+		});
+		instrumentation.waitForIdleSync();
+
+		instrumentation.runOnMainSync(new Runnable() {
+			@Override
+			public void run() {
+				editClaimActivity.newDestDialog.getButton(
+						Dialog.BUTTON_POSITIVE).performClick();
+
+			}
+		});
+		instrumentation.waitForIdleSync();
+
+		editClaimActivity = (EditClaimActivity) instrumentation
+				.waitForMonitorWithTimeout(activityMonitor, 10000);
+		assertNotNull(editClaimActivity);
+
+		final Button saveClaimButton = (Button) editClaimActivity
+				.findViewById(R.id.saveClaimButton);
+
+		instrumentation.runOnMainSync(new Runnable() {
+			@Override
+			public void run() {
+				saveClaimButton.performClick();
+
+			}
+		});
+
+		assertNotNull(claim.getDestinationList());
 	}
 
 	/*
-	 * US02.03.01 added 2015-03-23 () As a claimant, I want the list
-	 * of expense claims to have each claim color code by the distance of its
-	 * first destination geolocation to my home geolocation, so that claims for
+	 * US02.03.01 added 2015-03-23 () As a claimant, I want the list of expense
+	 * claims to have each claim color code by the distance of its first
+	 * destination geolocation to my home geolocation, so that claims for
 	 * distant travel can be distinguished from claims for nearby travel.
 	 */
 	public void testClaimsColorCodedByGeolocations() {
-		
+		getLoginActivity();
+		final ListView listOfClaims = (ListView) claimantClaimsListActivity
+				.findViewById(R.id.claimsList);
+		ColorDrawable listviewColor = (ColorDrawable) listOfClaims
+				.getChildAt(0).getBackground();
+		int colorId = listviewColor.getColor();
+		assertNotNull(colorId);
+
 	}
 
 	/*
-	 * US04.09.01 added 2015-03-23 () As a claimant, I want to
-	 * optionally attach a geolocation to an editable expense item, so I can
-	 * record where an expense was incurred.
+	 * US04.09.01 added 2015-03-23 () As a claimant, I want to optionally attach
+	 * a geolocation to an editable expense item, so I can record where an
+	 * expense was incurred.
 	 */
 	public void testGeolocationAttachedToExpenseItem() {
+		getAddExpenseActivity();
+		ActivityMonitor activityMonitor = instrumentation.addMonitor(
+				OSMDroidMapActivity.class.getName(), null, false);
+		final Button selectGeolocationButton = (Button) editExpenseActivity
+				.findViewById(R.id.GeolocationButtonExpense);
+
+		instrumentation.runOnMainSync(new Runnable() {
+			@Override
+			public void run() {
+				selectGeolocationButton.performClick();
+			}
+		});
+		instrumentation.waitForIdleSync();
+		osmDroidMapActivity = (OSMDroidMapActivity) instrumentation
+				.waitForMonitorWithTimeout(activityMonitor, 10000);
+		assertNotNull(osmDroidMapActivity);
+
+		final Button buttonSetlocation = (Button) osmDroidMapActivity
+				.findViewById(R.id.gps);
+
+		instrumentation.runOnMainSync(new Runnable() {
+			@Override
+			public void run() {
+				buttonSetlocation.performClick();
+			}
+		});
+		instrumentation.waitForIdleSync();
+
+		assertNotNull(expense.getLocation());
 
 	}
 
 	/*
-	 * US10.01.01 added 2015-03-23 () As a claimant, I want to set my
-	 * home geolocation.
+	 * US10.01.01 added 2015-03-23 () As a claimant, I want to set my home
+	 * geolocation.
 	 */
 	public void testSetHomeGeolocation() {
-		
+		getLoginActivity();
+
+		final Button homeGeolocationButton = (Button) claimantClaimsListActivity
+				.findViewById(R.id.HomeLocationButton);
+		ActivityMonitor activityMonitor = instrumentation.addMonitor(
+				OSMDroidMapActivity.class.getName(), null, false);
+
+		instrumentation.runOnMainSync(new Runnable() {
+			@Override
+			public void run() {
+				homeGeolocationButton.performClick();
+			}
+		});
+
+		osmDroidMapActivity = (OSMDroidMapActivity) instrumentation
+				.waitForMonitorWithTimeout(activityMonitor, 10000);
+		assertNotNull(osmDroidMapActivity);
+		final Button buttonSetlocation = (Button) osmDroidMapActivity
+				.findViewById(R.id.gps);
+		instrumentation.runOnMainSync(new Runnable() {
+			@Override
+			public void run() {
+				buttonSetlocation.performClick();
+			}
+		});
+		instrumentation.waitForIdleSync();
+
+		assertNotNull(user.getLocation());
 	}
 
 	/*
-	 * US10.02.01 added 2015-03-23 () As a claimant, I want to specify
-	 * a geolocation assisted by my mobile device (e.g., GPS) or manually using
-	 * a map.
+	 * US10.02.01 added 2015-03-23 () As a claimant, I want to specify a
+	 * geolocation assisted by my mobile device (e.g., GPS) or manually using a
+	 * map.
 	 */
 	public void testSpecifyGeolocationByMapGPS() {
+		getLoginActivity();
 
+		final Button GeolocationButton = (Button) claimantClaimsListActivity
+				.findViewById(R.id.HomeLocationButton);
+		ActivityMonitor activityMonitor = instrumentation.addMonitor(
+				OSMDroidMapActivity.class.getName(), null, false);
+
+		instrumentation.runOnMainSync(new Runnable() {
+			@Override
+			public void run() {
+				GeolocationButton.performClick();
+			}
+		});
+
+		osmDroidMapActivity = (OSMDroidMapActivity) instrumentation
+				.waitForMonitorWithTimeout(activityMonitor, 10000);
+		assertNotNull(osmDroidMapActivity);
+
+		final MapView map = (MapView) osmDroidMapActivity
+				.findViewById(R.id.map);
+
+		ViewAsserts.assertOnScreen(osmDroidMapActivity.getWindow()
+				.getDecorView(), map);
+
+		final Button buttonSetlocation = (Button) osmDroidMapActivity
+				.findViewById(R.id.gps);
+		instrumentation.runOnMainSync(new Runnable() {
+			@Override
+			public void run() {
+				buttonSetlocation.performClick();
+			}
+		});
+		instrumentation.waitForIdleSync();
+
+		assertNotNull(user.getLocation());
 	}
 
 	/*
-	 * US10.03.01 added 2015-03-23 () As a claimant, I want to view
-	 * any set or attached geolocation using a map.
+	 * US10.03.01 added 2015-03-23 () As a claimant, I want to view any set or
+	 * attached geolocation using a map.
 	 */
 	public void testViewGeolocationByMap() {
+		getLoginActivity();
 
+		final Button GeolocationButton = (Button) claimantClaimsListActivity
+				.findViewById(R.id.HomeLocationButton);
+		ActivityMonitor activityMonitor = instrumentation.addMonitor(
+				OSMDroidMapActivity.class.getName(), null, false);
+
+		instrumentation.runOnMainSync(new Runnable() {
+			@Override
+			public void run() {
+				GeolocationButton.performClick();
+			}
+		});
+
+		osmDroidMapActivity = (OSMDroidMapActivity) instrumentation
+				.waitForMonitorWithTimeout(activityMonitor, 10000);
+		assertNotNull(osmDroidMapActivity);
+
+		final MapView map = (MapView) osmDroidMapActivity
+				.findViewById(R.id.map);
+
+		ViewAsserts.assertOnScreen(osmDroidMapActivity.getWindow()
+				.getDecorView(), map);
 	}
 }
